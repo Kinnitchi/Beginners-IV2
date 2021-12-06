5631935f6c8a8799803be411b6fbe3afb184a9de
Kinnitchi <igorivieira97@gmail.com>
2021-12-06 00:05:54 -0300

    removido alguns cursos

---
+++ assets/js/apps.js
@@ -0,0 +1,8 @@
+var textoQueMuda = document.querySelector('#about p')
+
+var textoQueMuda = document.querySelector('#about p')
+
+textoQueMuda.addEventListener('click', function () {
+  textoQueMuda.innerHTML = '<a href="https://www.linkedin.com/in/kinnitchi/" id="class">Clique aqui e saiba mais</a>'
+})
+
